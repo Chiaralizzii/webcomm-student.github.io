bb3740cb5b8e1e4d9b46c574e8015f56446ab767
Chiaralizzii <chiara.lizzi@studenti.unimi.it>
2025-12-13 19:46:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    /* --- Script calendario --- */
+    const slides = document.querySelectorAll('.slide');
+    const wrapper = document.querySelector('.slider-container .slider-wrapper');
+
+    let index = 0;
+    const total = slides.length;
+
+    function showSlide(i) {
+        index = (i + total) % total; // ciclico
+        wrapper.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    setInterval(() => showSlide(index + 1), 3000);
+
+    showSlide(index);
+});
